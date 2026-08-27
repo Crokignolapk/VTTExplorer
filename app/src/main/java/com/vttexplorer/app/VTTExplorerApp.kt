78062f018d5272fc9318903b848dc50ceb199074
Crokignolapk <cpasuntest@gmail.com>
2026-08-27 19:23:21 +0200

    Init MapLibre in Application before MapView

--- app/src/main/java/com/vttexplorer/app/VTTExplorerApp.kt
+++ app/src/main/java/com/vttexplorer/app/VTTExplorerApp.kt
@@ -6,10 +6,17 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
+import org.maplibre.android.MapLibre
 
 class VTTExplorerApp : Application() {
     override fun onCreate() {
         super.onCreate()
+        // MapLibre DOIT être initialisé avant toute création de MapView
+        try {
+            MapLibre.getInstance(this)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
         startKoin {
             androidLogger(Level.ERROR)
             androidContext(this@VTTExplorerApp)
